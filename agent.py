a3d68ea976456a94efa21f9f060dda6e5149cdf6
Mateo Clémente <mateo.clemente@student.isae-supaero.fr>
2022-12-07 12:11:09 -0500

    chall

--- agent.py
+++ agent.py
@@ -20,13 +20,53 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime
 import numpy as np
-from DQN_heuristics import DQN, BestMove
 from avalam import *
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+        
 
 INF = 2**32-1
-
 TIME_PROP = [0.09,0.11,0.11,0.09,0.08,0.08,0.06,0.06]
+
+class DQN(nn.Module):
+
+    def __init__(self, h, w, outputs,device):
+        super(DQN, self).__init__()
+        self.device = device
+        self.conv1 = nn.Conv2d(1, 9 * 9, kernel_size=5)
+        self.bn1 = nn.BatchNorm2d(9 * 9)
+        self.conv2 = nn.Conv2d(9 * 9, 64, kernel_size=3, stride=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=2, stride=1)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size=3, stride=1):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w,5),3),2)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h,5),3),2)
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, outputs)
+
+
+    def forward(self, x):
+        x = x.to(self.device)
+        x = F.elu(self.bn1(self.conv1(x)))
+        x = F.elu(self.bn2(self.conv2(x)))
+        x = F.elu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
+
+
+class BestMove():
+    def __init__(self) -> None:
+        self.move = None
+
+    def update(self, act) -> None:
+        self.move = act
+
+
 
 class MyAgent(Agent):
 
@@ -221,7 +261,7 @@ def _play_action(state, action):
 
 
 
-# ----------- MAIN CALL -----------
+
 
 
 
